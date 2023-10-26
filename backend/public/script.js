e21f552781901f574224b10e1dc0ae83babf5942
Nepal Singh <nepalsss007@gmail.com>
2023-10-27 05:02:41 +0530

    finally integrated

---
+++ backend/public/script.js
@@ -0,0 +1,21 @@
+document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const userInput = document.getElementById("userInput").value;
+
+    // Make an API request to send user input and get a response
+    const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInput }),
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        document.getElementById("response").innerHTML = `<p>Response:</p><pre>${data.conversation}</pre>`;
+        console.log("responce");
+    } else {
+        console.error("Error:", response.status);
+    }
+});
